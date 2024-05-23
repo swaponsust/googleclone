4de0640e3e0fe76aa37ebd6c35496f3bc29e1977
Mahmudul Hasan <swaponmacpro@Mahmuduls-MacBook-Pro.local>
2024-05-24 01:50:39 +0600

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import HeaderHome from "@/components/HeaderHome";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
+      <HeaderHome />
       <p>Hello google</p>
     </main>
   );
